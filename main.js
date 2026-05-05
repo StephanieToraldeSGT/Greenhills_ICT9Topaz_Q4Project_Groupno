7252518855f92e8feb2461745d9fddc0657a0686
HermioneParedesHTP <paredes.hr@obmontessori.edu.ph>
2026-05-05 11:49:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function validateLogin() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    let correctUsername = "admin";
+    let correctPassword = "1234";
+
+   if (username === correctUsername && password === correctPassword) {
+        window.alert("Your username is: " + username + ", and your password is: " + password);
+    }
+    else if (username === password) {
+        window.alert("Username and password cannot be the same!");
+    }
+    else if (username.length < 5) {
+        window.alert("Username must be at least 5 characters long!");
+    }
+    else if (password.length < 5) {
+        window.alert("Password must be at least 5 characters long!");
+    }
+    else {
+        window.alert("Invalid username or password!");
+    }
+}
+
+function toggleInstructions() {
+    let instructions = document.getElementById("instructions");
+    let current = window.getComputedStyle(instructions).display;
+    if (current === "none") {
+        instructions.style.display = "block";
+    } else {
+        instructions.style.display = "none";
+    }
+}
+
+//
